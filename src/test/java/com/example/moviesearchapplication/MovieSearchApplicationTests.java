93faa7eaf2945344b2ede8b55bd789c435a41f97
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-04-05 18:42:14 +0100

    chore: boilerplate codes

---
+++ src/test/java/com/example/moviesearchapplication/MovieSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesearchapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
