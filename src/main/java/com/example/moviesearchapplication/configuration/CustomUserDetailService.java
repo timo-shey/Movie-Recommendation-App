2a57faabd37b3ea961b87f7a30849c61487b7458
Timileyin Adeleke <101255913+timo-shey@users.noreply.github.com>
2023-04-05 23:00:09 +0100

    chore: Java Mail Service Configured

--- src/main/java/com/example/moviesearchapplication/configuration/CustomUserDetailService.java
+++
@@ -1,2 +0,0 @@
-package com.example.moviesearchapplication.configuration;public class CustomUserDetailService {
-}
---
+++ src/main/java/com/example/moviesearchapplication/configuration/security/CustomUserDetailService.java
@@ -0,0 +1,4 @@
+package com.example.moviesearchapplication.configuration;
+
+public class CustomUserDetailService {
+}
